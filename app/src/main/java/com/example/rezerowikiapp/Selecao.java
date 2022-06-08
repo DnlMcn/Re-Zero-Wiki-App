2d8a51aa6d96de79e007b01e6e590058e12b8d79
Danilo Mancini <haomakk.himself@gmail.com>
2022-06-08 11:29:59 -0300

    App rodando no celular

--- app/src/main/java/com/example/rezerowikiapp/Selecao.java
+++ app/src/main/java/com/example/rezerowikiapp/Selecao.java
@@ -9,6 +9,6 @@ public class Selecao extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_selecao_pg1);
     }
 }
